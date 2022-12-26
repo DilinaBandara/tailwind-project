a7c1b998963421c9620eab3f245b4f86854da128
Dilina Bandara <dilinaedu@gmail.com>
2022-12-26 21:23:27 +0530

    Complete carding section edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        backg:"#10101a",
+        textc:"rgb(136, 146, 176)",
+        wrapper:"rgba(42,43,56,.5)",
+        greyf:"#c4c4c4",
+        borderc:"#2a2b37"
+      }
+    },
   },
   plugins: [],
 }
